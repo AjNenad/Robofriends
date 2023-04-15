241c6d2db27bb1c05a9329720247c217fa8361d5
AjNenad <116463101+AjNenad@users.noreply.github.com>
2023-04-15 09:07:23 +0200

    Add files via upload

---
+++ src/components/Robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Robot Robotic',
+    username: 'Bret',
+    email: 'Robot@Robotic.biz'
+  },
+  {
+    id: 2,
+    name: 'Kantoglavi',
+    username: 'Antonette',
+    email: 'Kantoglavi@biz.com'
+  },
+  {
+    id: 3,
+    name: 'Kolacic',
+    username: 'Samantha',
+    email: 'Kolacic@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Glavator',
+    username: 'Karianne',
+    email: 'Glavator@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Terminator',
+    username: 'Kamren',
+    email: 'Terminator@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Rocky',
+    username: 'Leopoldo_Corkery',
+    email: 'Rocky@jasper.info'
+  },
+  {
+    id: 18,
+    name: 'Lilyta',
+    username: 'Elwyn.Skiles',
+    email: 'Lilyta@billy.biz'
+  },
+  {
+    id: 20,
+    name: 'Masinator',
+    username: 'Maxime_Nienow',
+    email: 'Masinator@rosamond.me'
+  },
+  {
+    id: 91,
+    name: 'Urotron',
+    username: 'Delphine',
+    email: 'Urotron@dana.io'
+  },
+  {
+    id: 100,
+    name: 'TataCoop',
+    username: 'Moriah.Stanton',
+    email: 'Tatacoop@karina.biz'
+  },
+  {
+    id: 61,
+    name: 'RoboMama',
+    username: 'Moriah.Stanton',
+    email: 'RoboMama@karina.biz'
+  }
+];
